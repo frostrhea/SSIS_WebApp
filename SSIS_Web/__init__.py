0d2ad091339737487e14ec4bda3010ce4e73b17d
Rhea Salve Guingao <84138604+frostrhea@users.noreply.github.com>
2023-10-06 00:39:01 +0800

    refactor: project structure

--- SSIS_Web/__init__.py
+++ SSIS_Web/__init__.py
@@ -4,8 +4,14 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'frost'
     
-    @app.route('/')
-    def home():
-        return "ssis test!"
+    from .views import views
+    from .auth import auth
+    
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/auth')
+    
+    #@app.route('/')
+    #def home():
+     #           return "ssis test!"
     
     return app
