173b7b33df2287dc67f16e5ea197e44ae7eea86e
Rhea Salve Guingao <84138604+frostrhea@users.noreply.github.com>
2023-10-10 22:15:53 +0800

    feat: add HTML files, update views

--- SSIS_Web/views.py
+++ SSIS_Web/views.py
@@ -5,3 +5,11 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return render_template('student.html')
+
+@views.route('/courses')
+def course():
+    return render_template('course.html')
+
+@views.route('/colleges')
+def college():
+    return render_template('college.html')
