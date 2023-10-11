93e95b19fb015e733deda92f7d2a29c9d2044274
Rhea Salve Guingao <84138604+frostrhea@users.noreply.github.com>
2023-10-11 22:54:41 +0800

    chore: update pipfile dependencies to match req

--- SSIS_Web/__init__.py
+++ SSIS_Web/__init__.py
@@ -1,14 +1,43 @@
 from flask import Flask
+from flask_mysql_connector import MySQL
+from flask_bootstrap import Bootstrap
+from config import DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST, SECRET_KEY, BOOTSTRAP_SERVE_LOCAL
+from flask_wtf.csrf import CSRFProtect
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'frost'
+mysql = MySQL()
+bootstrap = Bootstrap()
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY=SECRET_KEY,
+        MYSQL_USER=DB_USERNAME,
+        MYSQL_PASSWORD=DB_PASSWORD,
+        MYSQL_DATABASE=DB_NAME,
+        MYSQL_HOST=DB_HOST,
+        #BOOTSTRAP_SERVE_LOCAL=BOOTSTRAP_SERVE_LOCAL
+    )
+    bootstrap.init_app(app)
+    mysql.init_app(app)
+    CSRFProtect(app)
     
     from .views import views
-    from .auth import auth
     
     app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
+    
+    
+    #from .user import user_bp as user_blueprint
+    #app.register_blueprint(user_blueprint)
+    
+#def create_app():
+#    app = Flask(__name__)
+#    app.config['SECRET_KEY'] = 'frost'
+    
+#    from .views import views
+    #from .auth import auth
+    
+#     app.register_blueprint(views, url_prefix='/')
+    #app.register_blueprint(auth, url_prefix='/')
     
     #@app.route('/')
     #def home():
