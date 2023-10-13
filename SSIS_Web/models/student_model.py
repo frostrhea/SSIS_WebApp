b376be2b5a876e28a13f6bf9f4d7bf68b6576a84
Rhea Salve Guingao <84138604+frostrhea@users.noreply.github.com>
2023-10-14 01:54:52 +0800

    feat: establish student db connection

--- SSIS_Web/models/student_model.py
+++ SSIS_Web/models/student_model.py
@@ -3,13 +3,18 @@ from flask_mysql_connector import MySQL
 mysql = MySQL()
 
 def get_student_data():
-    if not mysql.connection.is_connected():
-        return "Database connection is not established!"
-    
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM student_info")
-    fetchdata = cur.fetchall()
-    cur.close()
-    
-    return fetchdata
+    try:
+        if not mysql.connection.is_connected():
+            print("Database connection is not established!")
+            return "Database connection is not established!"
+
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM student_info")
+        fetchdata = cur.fetchall()
+        cur.close()
+
+        return fetchdata
+    except Exception as e:
+        print("Error executing SQL query:", e)
+        return "Error executing SQL query: " + str(e)
 
--- SSIS_Web/views/student_controller.py
+++ SSIS_Web/views/student_controller.py
@@ -1,9 +1,13 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify, request
 from SSIS_Web.models.student_model import get_student_data
 
 student_bp = Blueprint('student', __name__)
 
-@student_bp.route('/students')
+@student_bp.route('/students', methods=['GET'])
 def list_students():
-    students = get_student_data()
-    return render_template('student.html', data=students)
+    if request.headers.get('accept') == 'application/json':
+        students = get_student_data()
+        return jsonify(students)
+    else:
+        return render_template('student.html')
+    
