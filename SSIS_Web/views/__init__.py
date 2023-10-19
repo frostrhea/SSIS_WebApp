9ded3cff63d84d960556b5948d52b446aba2fe2f
Rhea Salve Guingao <84138604+frostrhea@users.noreply.github.com>
2023-10-20 05:01:39 +0800

    chore: remove unused files

--- SSIS_Web/views/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-#student_bp = Blueprint('student', __name__)
-course_bp = Blueprint('course', __name__)
-college_bp = Blueprint('college', __name__)
-
-
-#from . import controller
